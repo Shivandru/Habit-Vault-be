3938cdc06cf569406ba5de84d967a44cb61a73e6
Shivandru <shivandru.singh045@gmail.com>
2025-05-19 13:51:58 +0530

    changes

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "habit-vault-fe.vercel.app",
+    // origin: "http://localhost:5173",
     credentials: true,
-    // httpOnly: true
+    httpOnly: true
   })
 );
 
